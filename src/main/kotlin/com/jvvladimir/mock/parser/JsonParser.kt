4ab471d5d1ff07b4c181328efab58591f9b66e61
JVVladimir <vakashitsyn@yandex.ru>
2021-10-13 17:14:47 +0300

    add json support

--- src/main/kotlin/com/jvvladimir/mock/parser/JsonParser.kt
+++ src/main/kotlin/com/jvvladimir/mock/parser/JsonParser.kt
@@ -1,17 +1,24 @@
 package com.jvvladimir.mock.parser
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.jvvladimir.mock.model.Endpoints
+import com.jvvladimir.mock.validation.UriValidator
 import org.springframework.stereotype.Component
-import java.nio.file.Path
 
 @Component
-class JsonParser : Parser {
+class JsonParser(
+    val uriValidator: UriValidator,
+    val objectMapper: ObjectMapper
+) : Parser {
 
     override fun decode(rawConfig: String): Endpoints {
-        TODO("Not yet implemented")
+        val config = objectMapper.readValue<Endpoints>(rawConfig)
+        config.endpoints.forEach {
+            uriValidator.validate(it.request.uri)
+        }
+        return config
     }
 
-    override fun encode(endpoints: Endpoints): String {
-        TODO("Not yet implemented")
-    }
+    override fun encode(endpoints: Endpoints): String = objectMapper.writeValueAsString(endpoints)
 }
